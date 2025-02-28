4e5f27136951de4af6a3ff1f3c8fedc549b5cd78
Dominick Dickerson <ddyhw@umsystem.edu>
2025-02-27 19:31:02 -0600

    exclude password field

--- capstone2-server/accounts/serializers.py
+++ capstone2-server/accounts/serializers.py
@@ -4,7 +4,7 @@ from .models import Role, User
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = '__all__'
+        exclude = ['password']  # Explicitly exclude the password field
 
 class RoleSerializer(serializers.ModelSerializer):
     class Meta:
