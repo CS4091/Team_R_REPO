75e1311508bf012a28ebf8ec49f96daafdfa4d5b
Dominick Dickerson <ddyhw@umsystem.edu>
2025-03-03 13:26:18 -0600

    Testing two changes for Jira tickets, scrum-1 and scrum-2.

--- capstone2-server/accounts/serializers.py
+++ capstone2-server/accounts/serializers.py
@@ -4,7 +4,9 @@ from .models import Role, User
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        exclude = ['password']  # Explicitly exclude the password field
+        fields = ['id', 'email', 'first_name', 'last_name', 'is_active',
+                 'is_staff', 'username', 'last_login', 'is_superuser',
+                 'groups', 'user_permissions']
 
 class RoleSerializer(serializers.ModelSerializer):
     class Meta:
